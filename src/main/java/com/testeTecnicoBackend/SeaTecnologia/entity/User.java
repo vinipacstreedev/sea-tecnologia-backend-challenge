23e56aae487d90a0cac6405961bb1ec711b2bb1f
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-06-04 00:43:56 -0300

    Users and UserRepository

---
+++ src/main/java/com/testeTecnicoBackend/SeaTecnologia/entity/User.java
@@ -0,0 +1,41 @@
+package com.testeTecnicoBackend.SeaTecnologia.entity;
+
+import com.testeTecnicoBackend.SeaTecnologia.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/testeTecnicoBackend/SeaTecnologia/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.testeTecnicoBackend.SeaTecnologia.repository;
+
+import com.testeTecnicoBackend.SeaTecnologia.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
